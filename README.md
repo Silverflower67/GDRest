c7c9215485fa6eb234298095ef748a84ac998cb4
Silverflower67 <sofimaturana830@gmail.com>
2021-09-15 11:37:00 -0300

    More progress

---
+++ README.md
@@ -0,0 +1,9 @@
+# GDREST
+_____
+A REST API for Geometry Dash, made with Python, gd.py and Starlette
+
+## Get started
+###Install
+Install GDRest using pip:  
+`pip install gdrest`  
+or directly from the repo:
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+readme = open("README.md", "r").read()
+requirements = open("requirements.txt", "r").read().strip().splitlines()
+
+setup(
+    name='GDRest',
+    version='1.0',
+    packages=["levels", "me", "songs", "users"],
+    url='',
+    license='MIT',
+    author='Silverflower67',
+    author_email='',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    description='REST API for Geometry Dash, made with Python, gd.py and Starlette',
+    python_requires=">=3.8",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Natural Language :: English",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=requirements
+)
